284eddae8275720872c5178998cff5174a84b04f
RedStrikeRF <shubin152@gmail.com>
2024-09-29 16:06:03 +0500

    create simple form

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,8 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Login from "./components/login";
+import SignUp from "./components/signup";
+ 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+        <Navbar/>
+        <BrowserRouter>
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<SignUp />} />
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
-
+  
 export default App;
